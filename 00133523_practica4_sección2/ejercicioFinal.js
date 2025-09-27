4e25c679474ca2b9027d30854e0915feda1d51aa
DeniseMaldonadoG <00133523@uca.edu.sv>
2025-09-26 22:37:39 -0600

    feat: ejercicio final

--- 00133523_practica4_sección2/ejercicioFinal.js
+++ 00133523_practica4_sección2/ejercicioFinal.js
@@ -4,8 +4,26 @@ console.dir(window);
 console.dirxml(document.body);
 
 
-const marvel = ['Spider Man', 'Iron Man', 'Capitan America', 'Lobezno'];
-console.group('Superheroes de Marvel');
-marvel.forEach(superhero => console.log(superhero));
-console.groupEnd();
+document.getElementById("btnRecordar").addEventListener("click", recordarActividad);
 
+function recordarActividad() {
+    let dia = document.getElementById("dia").value.trim().toLowerCase();
+    let mensaje = document.getElementById("mensaje").value.trim();
+
+    const actividades = {
+        "lunes": "Debo atender un cliente especifico.",
+        "martes": "Visito una agencia fuera de la ciudad.",
+        "miercoles": "Debo llevar a mi hija al ballet (balé).",
+        "jueves": "Debo prioriza entregas de desarrollo.",
+        "viernes": "Debo atender problemas de manera remota.",
+        "sabado": "Debo hacer lo que mi esposa quiera."
+    };
+
+    let actividad = actividades[dia] || "No hay actividades registradas para este día.";
+
+    let resultado = "El dia de hoy es " + dia.charAt(0).toUpperCase() + dia.slice(1) + ". " +
+        actividad + "<br>" +
+        "Recordatorio: " + mensaje;
+
+    document.getElementById("resultado").innerHTML = resultado;
+}
